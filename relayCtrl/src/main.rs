c702343f37c65eb084b5ffa1f4f2fe4ff053744c
Slava Maslov <maslovw@gmail.com>
2023-04-02 00:04:37 +0400

    Add rust version of relay control CLI

---
+++ relayCtrl/src/main.rs
@@ -0,0 +1,110 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::time::Duration;
+use anyhow::{Context, Result};
+use log::{debug, error, info, warn};
+use serde::Deserialize;
+use serialport::{SerialPort, SerialPortBuilder};
+use structopt::StructOpt;
+use toml::Value;
+
+#[derive(StructOpt)]
+struct Opt {
+    relay_name: String,
+    action: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    relay: String,
+    baudrate: u32,
+    relays: HashMap<String, u8>,
+}
+
+impl Config {
+    fn from_file(file_path: &str) -> Result<Self> {
+        let config_str = fs::read_to_string(file_path)?;
+        let config: Config = toml::from_str(&config_str)?;
+        Ok(config)
+    }
+}
+
+struct Control {
+    relays: HashMap<String, u8>,
+    connection: Connection,
+}
+
+impl Control {
+    pub fn new(config: &Config) -> Result<Self> {
+        let connection = Connection::new(&config.relay, config.baudrate)?;
+        let control = Control {
+            relays: config.relays.clone(),
+            connection,
+        };
+        Ok(control)
+    }
+
+    pub fn toggle_relay(&mut self, relay_name: &str) {
+        if let Some(&pin) = self.relays.get(relay_name) {
+            self.connection.send_command(8, pin);
+        } else {
+            warn!("Toggle unknown relay: {}", relay_name);
+        }
+    }
+}
+
+struct Connection {
+    serial_port: Box<dyn SerialPort>,
+}
+
+impl Connection {
+    pub fn new(port: &str, baudrate: u32) -> Result<Self> {
+        let mut serial_port = serialport::new(port, baudrate).open().expect("Failed to open port");
+        serial_port.set_timeout(Duration::from_secs(5))?;
+
+        Ok(Self { serial_port })
+    }
+
+    pub fn send_command(&mut self, command: u8, data: u8) {
+        let cmd = [command, 0, data, command ^ data];
+        if let Err(e) = self.serial_port.write_all(&cmd) {
+            error!("Send command '{}' failed: {}", command, e);
+        }
+
+        let mut resp = [0u8; 4];
+        if let Err(e) = self.serial_port.read_exact(&mut resp) {
+            error!("Read resp '{}' failed: {}", command, e);
+        }
+
+        let xor = resp[0] ^ resp[1] ^ resp[2];
+        if resp[3] != xor {
+            error!("Read resp '{}' failed: {:?}", command, resp);
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let config_path = "config.toml";
+    let config = Config::from_file(config_path)?;
+
+    let opt = Opt::from_args();
+    let mut control = Control::new(&config)?;
+
+    match opt.action.as_str() {
+        "set" => control.toggle_relay(&opt.relay_name),
+        "reset" => control.toggle_relay(&opt.relay_name),
+        "toggle" => control.toggle_relay(&opt.relay_name),
+        "on" => control.toggle_relay(&opt.relay_name),
+        "off" => control.toggle_relay(&opt.relay_name),
+        _ => {
+            eprintln!("Invalid action. Please use set, reset, toggle, on, or off.");
+            std::process::exit(1);
+        }
+    }
+
+    Ok(())
+}
+
